2f212a381dd12f3fcbb9c98fa4a926867d7b5d2c
syedasadabbas <asadabbassherazi@gmail.com>
2026-07-12 07:52:43 -0700

    Add index.ts

---
+++ index.ts
@@ -0,0 +1,6 @@
+import { registerRootComponent } from "expo";
+import App from "./App";
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App)
+// and sets up the environment for Expo Go and native builds.
+registerRootComponent(App);
